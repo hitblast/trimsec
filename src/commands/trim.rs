963adc8d34bb8401efbb1bfe52a69dec14fef5ab
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-09-24 21:30:24 +0600

    refit `TrimCmd` with new delegate arch via `TrimCmd::delegate`
    took some inspiration from `FitCmd::delegate`

--- src/commands/trim.rs
+++ src/commands/trim.rs
@@ -1,87 +1,77 @@
 use crate::{
-    args::TCmdContent,
-    core::{context::Ctx, time::ToStringTime, youtils::TYoutubeId},
+    args::Token,
+    core::{
+        context::Ctx,
+        time::{TDuration, ToStringTime},
+    },
 };
 use anyhow::{Result, bail};
 
-pub struct TrimCmd {
-    content: TCmdContent,
+pub struct TrimCmd<'a> {
+    ctx: &'a mut Ctx,
+    duration: TDuration,
     multiplier: f64,
 }
 
-impl TrimCmd {
-    #[must_use]
-    pub fn new(content: TCmdContent, multiplier: f64) -> Self {
+impl<'a> TrimCmd<'a> {
+    fn new(ctx: &'a mut Ctx, duration: TDuration, multiplier: f64) -> Self {
         Self {
-            content,
+            ctx,
+            duration,
             multiplier,
         }
     }
 
-    fn yt_fallback(&mut self, id: &TYoutubeId, ctx: &mut Ctx) -> Result<()> {
-        let max = ctx.kwargs.max_items();
-        match ctx.client()?.fetch_duration_from_id(id, max) {
-            Ok(dur) => {
-                self.content = TCmdContent::Raw(dur);
-                ctx.kwargs.unset_max_items();
-                self.run(ctx)?;
-            }
-            Err(e) => bail!("Fetching URL failed: {e}"),
-        }
+    pub fn delegate(ctx: &'a mut Ctx, tokens: Vec<Token>) -> Result<Vec<Self>> {
+        let cur_dur: Option<TDuration> = None;
+        let cur_mul: Option<f64> = None;
+        let runnables: Vec<Self> = Vec::new();
 
-        Ok(())
+        while let Some(tok) = tokens.iter().next() {}
+
+        Ok(runnables)
     }
 
-    pub fn run(&mut self, ctx: &mut Ctx) -> Result<()> {
+    pub fn run(&mut self) -> Result<()> {
         if self.multiplier == 1.0 {
             println!("Would finish in linear time as used a multiplier of 1x.");
             return Ok(());
         }
 
-        match &mut self.content {
-            TCmdContent::Raw(dur) => {
-                if ctx.kwargs.max_items() != 0 {
-                    bail!("--max-items cannot be used for regular durations.")
-                }
-
-                dur.trim(self.multiplier);
-
-                let remaining = crate::core::time::time_in_day_after(dur.seconds());
-                let saved = dur.saved_time().to_string_duration();
-
-                let message = [
-                    format!(
-                        "\nFinishes in: {} ",
-                        if dur.splits() > 1 {
-                            format!("{dur}")
-                        } else {
-                            dur.to_string()
-                        }
-                    ),
-                    if remaining != 0.0 {
-                        format!("Time in day left: {} ", remaining.to_string_duration())
-                    } else {
-                        "Cannot finish today.".to_string()
-                    },
-                    format!(
-                        "{}Saved {saved}!{}\n\nTrimmed for {} item(s).",
-                        ctx.style.boldgreen(),
-                        ctx.style.reset(),
-                        dur.splits()
-                    ),
-                ]
-                .join("\n");
-
-                println!("{message}");
-            }
-
-            TCmdContent::YouTube(e) => {
-                let id = e.clone();
-                return self.yt_fallback(&id, ctx);
-            }
-
-            _ => todo!(),
+        if self.ctx.kwargs.max_items() != 0 {
+            bail!("--max-items cannot be used for regular durations.")
         }
+
+        let dur = &mut self.duration;
+        dur.trim(self.multiplier);
+
+        let remaining = crate::core::time::time_in_day_after(dur.seconds());
+        let saved = dur.saved_time().to_string_duration();
+
+        let message = [
+            format!(
+                "\nFinishes in: {} ",
+                if dur.splits() > 1 {
+                    format!("{dur}")
+                } else {
+                    dur.to_string()
+                }
+            ),
+            if remaining != 0.0 {
+                format!("Time in day left: {} ", remaining.to_string_duration())
+            } else {
+                "Cannot finish today.".to_string()
+            },
+            format!(
+                "{}Saved {saved}!{}\n\nTrimmed for {} item(s).",
+                self.ctx.style.boldgreen(),
+                self.ctx.style.reset(),
+                dur.splits()
+            ),
+        ]
+        .join("\n");
+
+        println!("{message}");
 
         Ok(())
     }
